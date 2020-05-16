d1840419449193380db3122068a5c4e387885149
jtown970 <jasonrtowner@gmail.com>
2020-05-16 08:13:42 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
+import List from './components/Lists'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      userList: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get(`https://virtserver.swaggerhub.com/HopeIgetFree/grocery/1.0.0/pet/findByStatus?status=available`)
+    .then(res => {
+      this.setState({
+        userList: res.data
+      })
+    })
+  }
+
+  render(){
+    const {userList} = this.state;
+    console.log(userList)
+    return (
+      <div className="App">
+        <h1 className="company-name">YSHOP</h1>
+        <List/>
+        {
+          userList.map(pets => {
+            return <List key={pets.id}/>
+          })
+        }
+
+      </div>
+    );
+  }
 }
+
 
 export default App;
